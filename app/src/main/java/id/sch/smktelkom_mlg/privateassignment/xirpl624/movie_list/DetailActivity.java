01d72bb513a5704377990f4e5a2f22b6804229b6
rizky30 <rizky.ajie30@gmail.com>
2017-05-14 23:34:36 +0700

    Add Color Add Button Back

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl624/movie_list/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl624/movie_list/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,5 +46,11 @@ public class DetailActivity extends AppCompatActivity {
         vote.setText(vot);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
